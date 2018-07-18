355d4f1a66a7107cddfafb49838960e26b96048f
jiashuyu <jsysincere@163.com>
2018-07-18 15:38:15 +0800

    小练习

---
+++ run.sh
@@ -0,0 +1,11 @@
+mvn clean package
+echo "--------deson1-----------"
+java -jar deson1/target/deson1.jar
+echo "--------deson2-----------"
+java -jar deson2/target/deson2.jar
+echo "--------deson3-----------"
+java -jar deson3/target/deson3.jar
+echo "--------deson4-----------"
+java -jar deson4/target/deson4.jar
+echo "--------deson5-----------"
+java -jar deson5/target/deson5.jar
